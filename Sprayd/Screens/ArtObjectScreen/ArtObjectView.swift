87af5f35a3d82d665e35474c19c0b0dcbe9bf754
lzkgmr <lzkgmr@gmail.com>
2026-04-02 11:31:02 +0300

    Fix CI iOS target

--- Sprayd/Screens/ArtObjectScreen/ArtObjectView.swift
+++ Sprayd/Screens/ArtObjectScreen/ArtObjectView.swift
@@ -18,7 +18,10 @@ struct ArtObjectView: View {
                 VStack(alignment: .leading, spacing: 10) {
                     Text("Name of Art")
                         .font(Font.InstrumentBold22)
-                    Text("73 is the 21-st prime number. Its mirror, 37, is the 12-th, and its mirror, 21, is the product of multiplying, hang on to your hats, 7 and 3.")
+                    Text(
+                        "73 is the 21-st prime number. Its mirror, 37, is the 12-th, and its mirror, 21, " +
+                        "is the product of multiplying, hang on to your hats, 7 and 3."
+                    )
                         .foregroundStyle(Color(.secondaryLabel))
                         .font(Font.InstrumentMedium16)
                 }
@@ -28,7 +31,7 @@ struct ArtObjectView: View {
                 .padding(.bottom, 20)
             }
         }
-        
+
     }
 }
 
