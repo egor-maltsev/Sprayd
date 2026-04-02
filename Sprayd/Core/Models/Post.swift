fbf5f288f528159f2d8ccb9b5e1454af8953c8b0
loxxych <kate.coconutt@gmail.com>
2026-04-02 20:53:09 +0300

    progress on the profile UI

---
+++ Sprayd/Core/Models/Post.swift
@@ -0,0 +1,10 @@
+//
+//  Post.swift
+//  Sprayd
+//
+//  Created by loxxy on 02.04.2026.
+//
+
+// TODO: - Finish model description
+struct Post {
+}
--- Sprayd/Screens/MyProfileScreen/MyProfileView.swift
+++ Sprayd/Screens/MyProfileScreen/MyProfileView.swift
@@ -11,11 +11,17 @@ struct ProfileView: View {
     // MARK: - Constants
     private enum Const {
         // Strings
-        static let buttonBottomText: String = "Add new street"
+        static let postedButtonBottomText: String = "Add new seen art"
+        
+        static let vButtonBottomText: String = "Add new seen art"
+        
+        static let postedSectionText: String = "Posted"
+        static let visitedSectionText: String = "Visited"
         
         // UI constraint properties
         static let profileImageSize: CGFloat = 160
         static let profileImageCornerRadius: CGFloat = profileImageSize / 2
+        static let usernameDescriptionSpacing: CGFloat = 13
         
         // Fonts
         static let usernameFont: Font = .custom("Climate Crisis", size: 22)
@@ -27,9 +33,62 @@ struct ProfileView: View {
     
     // MARK: - Fields
     @State private var selectedOption = "Posted"
-    
+    private var posts: [Post]?
+
     // MARK: - Subviews
+    private var bioView: some View {
+        VStack {
+            Image(systemName: "person.crop.circle.fill")
+                .resizable()
+                .frame(width: Const.profileImageSize, height: Const.profileImageSize)
+                .cornerRadius(Const.profileImageCornerRadius)
+                .frame(maxWidth: .infinity)
+            VStack(spacing: Const.usernameDescriptionSpacing) {
+                HStack {
+                    Text("Username")
+                        .font(Const.usernameFont)
+                    Image(systemName: "pencil")
+                }
+                .frame(maxWidth: .infinity)
+                
+                HStack {
+                    Text("Description")
+                        .font(Const.descriptionFont)
+                    Image(systemName: "pencil")
+                }
+                .frame(maxWidth: .infinity)
+            }
+        }
+    }
     
+    private var pickerView: some View {
+        Picker("", selection: $selectedOption) {
+            Text(Const.postedSectionText).tag(Const.postedSectionText)
+            Text(Const.visitedSectionText).tag(Const.visitedSectionText)
+        }
+        .pickerStyle(.segmented)
+        .padding(.horizontal)
+    }
+    
+    private var sectionTitle: some View {
+        Text(Const.postedSectionText)
+            .frame(maxWidth: 150)
+            .font(Const.sectionTitleFont)
+    }
+    
+    private var addButtonView: some View {
+        VStack(alignment: .center) {
+            AddButton()
+            
+            Text(Const.postedButtonBottomText)
+                .font(Const.buttonBottomTextFont)
+        }
+        .frame(maxWidth: .infinity)
+    }
+    
+    // TODO: - Replace with an array of posts
+    private var postView: some View = ArtMediumCardView()
+        
     // MARK: - Body
     var body: some View {
         ZStack {
@@ -38,45 +97,15 @@ struct ProfileView: View {
             
             ScrollView {
                 VStack(alignment: .leading, spacing: 16) {
-                    Image(systemName: "person.crop.circle.fill")
-                        .resizable()
-                        .frame(width: Const.profileImageSize, height: Const.profileImageSize)
-                        .cornerRadius(Const.profileImageCornerRadius)
-                        .frame(maxWidth: .infinity)
+                    bioView
                     
-                    HStack {
-                        Text("Username")
-                            .font(Const.usernameFont)
-                        Image(systemName: "pencil")
-                    }
-                    .frame(maxWidth: .infinity)
+                    pickerView
                     
-                    HStack {
-                        Text("Description")
-                            .font(Const.descriptionFont)
-                        Image(systemName: "pencil")
-                    }
-                    .frame(maxWidth: .infinity)
+                    sectionTitle
                     
-                    Picker("", selection: $selectedOption) {
-                        Text("Posted").tag("Posted")
-                        Text("Visited").tag("Visited")
-                    }
-                    .pickerStyle(.segmented)
-                    .padding(.horizontal)
+                    addButtonView
                     
-                    Text("Posted")
-                        .frame(maxWidth: 150)
-                        .font(Const.sectionTitleFont)
-                    
-                    VStack(alignment: .center) {
-                        AddButton()
-                        
-                        Text(Const.buttonBottomText)
-                            .font(Const.buttonBottomTextFont)
-                    }
-                    .frame(maxWidth: .infinity)
-                    
+                    postView
                 }
                 .frame(maxWidth: .infinity, alignment: .leading)
             }
