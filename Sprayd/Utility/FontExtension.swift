e98baf5f6790cae1624661105e47da85bde0abec
loxxych <kate.coconutt@gmail.com>
2026-04-03 15:20:15 +0300

    add instrument fonts

--- Sprayd/Utility/FontExtension.swift
+++ Sprayd/Utility/FontExtension.swift
@@ -9,9 +9,13 @@ import SwiftUI
 
 extension Font {
     static let InstrumentRegular13 = Font.custom("InstrumentSans-Regular", size: 13)
-    
+    static let InstrumentRegular18 = Font.custom("InstrumentSans-Regular", size: 18)
+
     static let InstrumentBold22 = Font.custom("InstrumentSans-Bold", size: 22)
+    static let InstrumentBold20 = Font.custom("InstrumentSans-Bold", size: 20)
     
     static let InstrumentMedium16 = Font.custom("InstrumentSans-Medium", size: 16)
     static let InstrumentMedium13 = Font.custom("InstrumentSans-Medium", size: 13)
+    static let InstrumentMedium18 = Font.custom("InstrumentSans-Medium", size: 18)
+    static let InstrumentMedium20 = Font.custom("InstrumentSans-Medium", size: 20)
 }
